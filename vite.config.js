7a21a57d947c6dcbdf32dfffcf789911f888b2e9
milad latifi <mila513d@gmail.com>
2023-08-12 17:54:18 +0330

    deploy-3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/productManager",
   plugins: [react()],
+  base: "/product-manager",
 });
